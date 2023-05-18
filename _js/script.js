8a7d99f4ba9cef87173e70bd4c4e9490d19d9f25
Vandinelson Rocha <vandInforma.tic@gmail.com>
2023-05-17 23:03:28 -0100

    working in the section main

--- _js/script.js
+++ _js/script.js
@@ -1,20 +1,34 @@
+//--------------EFEITO DE SCROLL NO HEADER--------------
+
 const header = document.querySelector('header');
 const logo = document.querySelector('#logo');
 
-/*Criar uma função de scroll que executará algumas coisas qd fizer scroll*/
 window.addEventListener("scroll", diminuirHeader);
 
 function diminuirHeader()  {
-    //Se for feito um scroll maior que 100px e o header tiver a classe max
     if(window.pageYOffset > 100 && header.classList.contains("max"))  {
-        header.classList.remove("max"); //remove-se a classe max
-        header.classList.add("min"); //coloca a classe min com seus estilos definidos no css
-        logo.style.fontsize = "300px"; //e a imagem diminui a largura
+        header.classList.remove("max"); 
+        header.classList.add("min"); 
+        logo.style.fontSize = "25px";
     }
-    //Se não tiver feito nenhum scroll e o header estiver com a classe min 
     else if(window.pageYOffset == 0 && header.classList.contains("min"))  {
-        header.classList.add("max"); //tem de voltar a classe max
-        header.classList.remove("min"); //remover a classe min
-        logo.style.fontsize = "300px"; //e a imagem retorna para a largura que está definida no css
+        header.classList.add("max"); 
+        header.classList.remove("min"); 
+        logo.style.fontSize = "32px"; 
     }
 }
+
+//--------------ESCRITA DINÂMICA - H2 -> APRESENTAÇÃO--------------
+const digitarH2 = document.querySelector('#digitando');
+function digitacaoLetras(elemento)  {
+    const tituloH2 = elemento.innerHTML.split('');
+
+    elemento.innerHTML = '';
+    tituloH2.forEach((letra, i) =>  {
+        setTimeout(() =>  {
+            elemento.innerHTML += letra;
+        }, 75 * i);        
+    });
+}
+
+digitacaoLetras(digitarH2);
