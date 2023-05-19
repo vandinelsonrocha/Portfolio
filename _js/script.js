e4526c0ec90fe274810b8e211b3f02089fac02a6
Vandinelson Rocha <vandInforma.tic@gmail.com>
2023-05-19 14:32:33 -0100

    implementation of light mode

--- _js/script.js
+++ _js/script.js
@@ -1,3 +1,17 @@
+//--------------LIGHT MODE//--------------
+const moon = document.getElementById('moon');
+
+moon.onclick = function()  {
+  document.body.classList.toggle("light-mode");
+
+  if(document.body.classList.contains("light-mode"))  {
+    moon.src = "_img/moon.png";
+  }
+  else  {
+    moon.src = "_img/sun.png";
+  }
+}
+
 //--------------EFEITO DE SCROLL NO HEADER--------------
 
 const header = document.querySelector('header');
@@ -20,15 +34,23 @@ function diminuirHeader()  {
 
 //--------------ESCRITA DINÂMICA - H2 -> APRESENTAÇÃO--------------
 const digitarH2 = document.querySelector('#digitando');
-function digitacaoLetras(elemento)  {
-    const tituloH2 = elemento.innerHTML.split('');
 
-    elemento.innerHTML = '';
-    tituloH2.forEach((letra, i) =>  {
-        setTimeout(() =>  {
-            elemento.innerHTML += letra;
-        }, 75 * i);        
-    });
+function digitacaoLetras(elemento) {
+  const tituloH2 = elemento.innerHTML.split('');
+
+  elemento.innerHTML = '';
+  tituloH2.forEach((letra, i) => {
+    setTimeout(() => {
+      if (letra === '|') {
+        elemento.innerHTML += '<span style="color: white;">|</span>';
+      } else {
+        elemento.innerHTML += letra;
+        elemento.style.color = "#61DAFB";
+      }
+    }, 75 * i);
+  });
 }
 
 digitacaoLetras(digitarH2);
+
+  
